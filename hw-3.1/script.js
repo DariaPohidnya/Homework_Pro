f05f0e05a5458bf727cec2550e069811dd682473
DariaPohidnya <dadagame18@gmail.com>
2024-08-08 17:57:53 +0300

    done

---
+++ hw-3.1/script.js
@@ -0,0 +1,26 @@
+// Число
+console.log(typeof 42); 
+
+// Рядок
+console.log(typeof 'hello'); 
+
+// Булеве значення
+console.log(typeof true); 
+
+// Undefined
+console.log(typeof undefined); 
+
+// Null
+console.log(typeof null); 
+
+// Об'єкт
+console.log(typeof {name: 'Alice', age: 30}); 
+
+// Функція
+console.log(typeof function() {}); 
+
+// Символ
+console.log(typeof Symbol('symbol')); 
+
+// BigInt
+console.log(typeof 123n); 
---
+++ hw-3.2/index.js
@@ -0,0 +1,9 @@
+const name = "Daria";
+
+const lastName = "Pohidnya";
+
+const age = 20;
+
+const myInfo = `Name is ${name}, last name is ${lastName}, age is ${age}.`;
+
+console.log(myInfo)
